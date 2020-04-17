fa13cff2dc8ff65de0080e30767f4c7a06c7321e
maikl-maikl <maikljonson252@gmail.com>
2020-04-17 15:09:35 +0300

    commit commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.slider').slick({
+
+       infinite: true,
+       slidesToShow: 3,
+       slidesToScroll: 2,     
+       autoplay: false,
+       autoplaySpeed: 1000,
+	   
+	  
+});
--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 
- 
- $(".center").slick({
-        dots: true,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-
-
+$('.slider').slick({
+	 /* dots: true;
+	  infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,     
+      autoplay: true,
+      autoplayspeed: 500,*/
 });
+
 
 
 
