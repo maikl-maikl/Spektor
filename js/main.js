2987751d53ce28a62f685ca6cdcfb25f5caebdc0
maikl-maikl <maikljonson252@gmail.com>
2020-04-27 11:53:14 +0300

    commit commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 $('.slider').slick({
-
-       infinite: true,
+       speed: 500,
+       infinite: true,     
+       arrows: true, 
        slidesToShow: 3,
        slidesToScroll: 2,     
        autoplay: false,
        autoplaySpeed: 1000,	 
-       arrows: true,
-       
-    });
+       nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+       prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+
+});
 
 	  
